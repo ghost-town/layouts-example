4d4df59369f2dc7197853672cd33609163166263
Brian Woodward <brian.woodward@gmail.com>
2013-10-14 20:51:19 -0400

    Initial commit
    Setting up an example using the layout helpers with assemble.

---
+++ src/helpers/helper-example-layout-helpers.js
@@ -0,0 +1,17 @@
+(function() {
+  /**
+   * Handlebars Helpers: Layout Helpers
+   * Copyright (c) 2013 Jon Schlinkert, Brian Woodward, contributors
+   * Licensed under the MIT License (MIT).
+   */
+  'use strict';
+
+  /**
+   * These helpers are inspired by handlebars-layouts.
+   * https://github.com/shannonmoeller/handlebars-layouts * 
+   */
+
+
+  
+
+}).call(this);
